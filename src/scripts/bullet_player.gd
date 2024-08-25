ed21da7e4a823c363374d3e1635093e05aa95644
Luca Memeo <memeoluca6@gmail.com>
2024-08-25 19:16:58 +0200

    added bullets

---
+++ src/scripts/bullet_player.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var travelled_distance = 0
+
+func _physics_process(delta):
+	const speed = 1000
+	const weapon_range = 1200
+	var direction = Vector2(0,-1)
+	position += direction * speed * delta
+	
+	travelled_distance += speed * delta
+	if travelled_distance > weapon_range: 
+		queue_free()
+
+
+
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
