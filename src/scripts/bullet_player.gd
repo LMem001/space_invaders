4f29faa8cf92efcae77bdff9a7211f805190ddaf
Luca Memeo <memeoluca6@gmail.com>
2024-08-26 14:58:01 +0200

    added method to add enemies and now the player destroys enmemies with bullets

--- src/scripts/bullet_player.gd
+++ src/scripts/bullet_player.gd
@@ -5,14 +5,19 @@ var travelled_distance = 0
 func _physics_process(delta):
 	const speed = 1000
 	const weapon_range = 1200
-	var direction = Vector2(0,-1)
+	var direction = _bullet_direction()
 	position += direction * speed * delta
 	
 	travelled_distance += speed * delta
 	if travelled_distance > weapon_range: 
 		queue_free()
 
+func _bullet_direction():
+	return Vector2(0,-1)
+
 func _on_body_entered(body):
 	queue_free()
-	if body.has_method("take_damage"):
-		body.take_damage()
+	if body.has_method("_take_damage"):
+		body._take_damage()
+	else:
+		body.queue_free()
--- src/scripts/enemies.gd
+++ src/scripts/enemies.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+@export var red_enemy_scene : PackedScene  # enemy package
 var enemy_speed : float = 100.0
 var velocity = Vector2(180, 0)
 var bCollidedRight = false
+var num_red_enemies = 10
 
 func _physics_process(delta):
 	position += velocity * delta
@@ -10,9 +12,19 @@ func _physics_process(delta):
 		if enemy is CharacterBody2D:
 			enemy.move_and_slide()
 
+func _ready():
+	# Aggiungi nemici all'avvio della scena
+	add_enemies(num_red_enemies)
+
 func _move_down ():
 	velocity = Vector2(0, 90)
 	%MoveDownTimer.start()
+	
+func add_enemies(count):
+	for i in range(count):
+		var enemy_instance = red_enemy_scene.instantiate() 
+		enemy_instance.position = Vector2(100 + i * 50, 100) 
+		add_child(enemy_instance)
 
 func _on_wall_ritght_area_body_entered(body):
 	if body.is_in_group("gEnemy") && !bCollidedRight:
